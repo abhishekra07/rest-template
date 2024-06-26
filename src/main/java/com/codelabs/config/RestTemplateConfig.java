3825b08c8a1064302da86604325c7d5d04476133
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-06-26 17:00:47 +0530

    Create RestTemplateConfig.java

---
+++ src/main/java/com/codelabs/config/RestTemplateConfig.java
@@ -0,0 +1,46 @@
+package com.codelabs.config;
+
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.support.HttpRequestWrapper;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class RestTemplateConfig {
+
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+    @Bean
+    public RestTemplateCustomizer restTemplateCustomizer() {
+        return new RestTemplateCustomizer() {
+            @Override
+            public void customize(RestTemplate restTemplate) {
+                restTemplate.getInterceptors().add(baseUrlInterceptor());
+            }
+        };
+    }
+
+    private ClientHttpRequestInterceptor baseUrlInterceptor() {
+        return (request, body, execution) -> {
+            try {
+                URI uri = new URI(BASE_URL + request.getURI().toString());
+                request.getHeaders().set("Base-URL-Header", BASE_URL);
+                request = new HttpRequestWrapper(request) {
+                    @Override
+                    public URI getURI() {
+                        return uri;
+                    }
+                };
+            } catch (URISyntaxException e) {
+                throw new IOException(e);
+            }
+            return execution.execute(request, body);
+        };
+    }
+}
